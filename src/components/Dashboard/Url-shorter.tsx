5b9b3a5ae2f71a722492a0cdcee99d586df63487
Moein Hosseini <moein.hoseini92@gmail.com>
2024-11-08 15:02:33 +0330

    add env

--- src/components/Dashboard/Url-shorter.tsx
+++ src/components/Dashboard/Url-shorter.tsx
@@ -1,6 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ChartOne from "../Charts/ChartOne";
 import ChartTwo from "../Charts/ChartTwo";
 import ChatCard from "../Chat/ChatCard";
@@ -18,11 +18,9 @@ const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
 });
 
 const UrlShortner: React.FC = () => {
-  const { status } = useSession();
-  const [value, setValue] = useState('');
-  const [shortenLink, setShortenLink] = useState('');
-
-
+  const { data: session, status } = useSession();
+  const [value, setValue] = useState("");
+  const [shortenLink, setShortenLink] = useState("");
 
   if (status === "unauthenticated") {
     return redirect("/auth/signin");
@@ -32,21 +30,22 @@ const UrlShortner: React.FC = () => {
     setValue(event.target.value);
   };
 
-  const handleSubmit = async() => {
+  const handleSubmit = async () => {
     try {
-      const res = await fetch(`http://5.161.100.20:8080/shorten`,{
-        method:"POST",
-        body: JSON.stringify({ Original: value }),
-
+      const res = await fetch(`http://5.161.100.20:8080/shorten`, {
+        method: "POST",
+        body: JSON.stringify({
+          Original: value,
+          userEmail: session?.user?.email,
+        }),
       });
       const data = await res.json();
-      setShortenLink(data)
+      setShortenLink(data);
       console.log(data);
     } catch (err) {
       console.log(err);
     }
   };
-
 
   return (
     <div className="flex min-h-[48rem] ">
@@ -61,7 +60,9 @@ const UrlShortner: React.FC = () => {
               placeholder="Enter your Url here ..."
               aria-label="Full url"
             />
-            <input type="submit" value="Submit" 
+            <input
+              type="submit"
+              value="Submit"
               className="flex-shrink-0 rounded border-4 border-teal-500 bg-teal-500 px-2 py-1 text-sm text-white hover:border-teal-700 hover:bg-teal-700"
             >
               Make it shorter!!!
@@ -69,7 +70,9 @@ const UrlShortner: React.FC = () => {
           </div>
         </form>
         <div className="mt-5 h-4 p-3">
-          <h1>{shortenLink ? shortenLink : 'Result will displayed here ...'}</h1>
+          <h1>
+            {shortenLink ? shortenLink : "Result will displayed here ..."}
+          </h1>
         </div>
       </div>
     </div>
